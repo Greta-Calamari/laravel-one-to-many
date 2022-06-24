59d96e842b38491b34e27d3e078c1bb0d6158449
ladykakki <greta_calamari@hotmail.it>
2022-06-24 12:50:08 +0200

    required sull input

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,7 +7,7 @@
             
             <div class="mb-3">
                     <label for="title" class="form-label">title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}" placeholder="inserisci titolo">
+                    <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}" placeholder="inserisci titolo" required>
             </div>
 
             
