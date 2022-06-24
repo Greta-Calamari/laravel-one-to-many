a96a40889185ee098d5b288d5bd86d12b39ef736
ladykakki <greta_calamari@hotmail.it>
2022-06-24 10:21:04 +0200

    collegati i 2 modele creata migration e seeder per i tags

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,7 +4,14 @@
 @section('content')
 
         <h1>{{$post->title}}</h1>
-        {{-- <h2>{{$post->category->name}}</h2> --}}
+
+        {{-- <h2>{{$post->category ? $post->category->name : '-'}}</h2> --}}
+        @if($post->category != null)
+        <h2>Categoria: {{$post->category->name}}</h2>
+        @else
+        <h2>Nessuna categoria assegnata</h2>
+        @endif
+
         <small>{{$post->created_at}}</small>
         <p>{{$post->content}}</p>
         <h5>Pubblicato: {{$post->published}}</h5>
